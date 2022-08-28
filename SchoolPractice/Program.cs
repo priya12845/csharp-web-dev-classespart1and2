0892aa988a2fb1707ed51c95c801e26f9a484163
priya12845 <priya12845@gmail.com>
2022-08-28 17:03:13 -0500

    added one more line

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,7 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello  World");
+            //Added a new line
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
         }
     }
